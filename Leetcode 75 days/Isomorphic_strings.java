fb46ec99e8597a730414a9e65fe62dba60d470c7
Kajal Varma <46535714+kajal148@users.noreply.github.com>
2022-07-17 01:44:58 +0530

    Create Isomorphic_strings.java

---
+++ Leetcode 75 days/Isomorphic_strings.java
@@ -0,0 +1,36 @@
+// Isomorphic Strings - 205
+// Link - https://leetcode.com/problems/isomorphic-strings/
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int ls = s.length();
+        int ts = s.length();
+        
+        if(ls != ts){
+            return false;
+        }
+        
+        HashMap< Character , Character > map1 = new HashMap<>();
+        HashMap< Character , Boolean > map2 = new HashMap<>();
+    
+        for(int i = 0 ; i < s.length();i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            if(map1.containsKey(ch1) == true){
+                if(map1.get(ch1) != ch2){
+                    return false;
+                }
+            }else{
+                if(map2.containsKey(ch2) == true){
+                    return false;
+                }else{
+                    map1.put(ch1 , ch2);
+                    map2.put(ch2 , true);
+                }
+            }
+
+        }
+        return true;
+    }
+}
